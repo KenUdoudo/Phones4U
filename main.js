5d0641b5c3965f50d5f322a6154d0c4fe6164eb7
Ken <kendo302003@gmail.com>
2023-03-03 15:10:42 +0100

    it fucking worked

---
+++ main.js
@@ -0,0 +1,8 @@
+const discount = document.getElementById('discount')
+const mainFeatured = document.getElementById('one')
+const mainDiscount = document.getElementById('two')
+
+function switchOne(){
+ mainFeatured.style.display = "none"
+ mainDiscount.style.display = "block"
+}
