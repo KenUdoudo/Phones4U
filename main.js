47ad336657157d8325d5c2d63eff0935407993e1
Ken <kendo302003@gmail.com>
2023-03-06 07:44:12 +0100

    color gradient

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const discount = document.getElementById('discount')
-const featured = document.getElementById('featured')
+const news = document.getElementById('news')
 const collection = document.getElementById('collection')
 
-const mainFeatured = document.getElementById('one')
-const mainDiscount = document.getElementById('two')
+const mainDiscount = document.getElementById('one')
+const mainNews = document.getElementById('two')
 const mainCollection = document.getElementById('three')
 
 
 function switchOne(){
-  mainFeatured.style.display = "block"
-  mainDiscount.style.display = "none"
+  mainDiscount.style.display = "block"
+  mainNews.style.display = "none"
   mainCollection.style.display = "none"
 }
 function switchTwo(){
- mainFeatured.style.display = "none"
- mainDiscount.style.display = "block"
+ mainDiscount.style.display = "none"
+ mainNews.style.display = "block"
  mainCollection.style.display = "none"
 }
 function switchThree(){
-  mainFeatured.style.display = "none"
   mainDiscount.style.display = "none"
+  mainNews.style.display = "none"
   mainCollection.style.display = "block"
  }
 
